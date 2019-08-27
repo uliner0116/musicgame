7b37b1f7767870e3e6b84f258d2cbf5d230c296d
z01329125 <z01329125@gmail.com>
2019-08-28 04:09:35 +0800

    ssss

---
+++ musicgame/Assets/Scripts/ListControll.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ListControll : MonoBehaviour
+{
+
+    public RectTransform[] pages;
+    public Vector2[] targetPosition;
+
+    int current = 0;
+    bool locked = false;
+    Vector2 sampleSize;
+
+    void Start()
+    {
+        sampleSize = GetComponent<RectTransform>().sizeDelta;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Next();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Previous();
+        }
+        Refresh();
+    }
+
+    void Next()
+    {
+        if (locked)
+            return;
+
+        targetPosition[current].x = 1920f;
+
+        current++;
+        if (current >= pages.Length)
+            current = 0;
+
+        pages[current].anchoredPosition = new Vector2(-1920f, 0);
+        targetPosition[current].x = 0;
+        StartCoroutine("Lock");
+    }
+    void Previous()
+    {
+        if (locked)
+            return;
+
+        targetPosition[current].x = -1920f;
+
+        current--;
+        if (current < 0)
+            current = pages.Length - 1;
+
+        pages[current].anchoredPosition = new Vector2(1920f, 0);
+        targetPosition[current].x = 0;
+        StartCoroutine("Lock");
+    }
+
+    void Refresh()
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            Vector3 pos = pages[i].anchoredPosition;
+            pos = Vector3.Lerp(pos, targetPosition[i], 0.2f);
+            pages[i].anchoredPosition = pos;
+        }
+    }
+
+    IEnumerator Lock()
+    {
+        locked = true;
+        yield return new WaitForSeconds(0.2f);
+        locked = false;
+    }
+
+}
