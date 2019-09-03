2a936861ce9627a47f9e41643bc304bd3ae844bb
uluner <uliner0116@gmail.com>
2019-09-04 05:16:31 +0800

    掛哥音量調整+難度的程式碼V1

--- musicgame/Assets/Scripts/List/ListButton.cs
+++ musicgame/Assets/Scripts/List/ListButton.cs
@@ -8,11 +8,37 @@ using UnityEngine.UI;
 
 public class ListButton : MonoBehaviour
 {
-    public int line;
+    int line;
+    string songName;
+    string level;
+    string lineName;
+    TextAsset songTxt;
+    GameObject father_gameObject;   //宣告一個GameObject(用來放取得的父物件)。
     //點擊時呼叫
     public void OnPathClick()
     {
-        Debug.Log("click!" + transform.name);
+        
+        //取得這首歌的名稱
+        father_gameObject = gameObject.transform.parent.gameObject;
+        father_gameObject = father_gameObject.transform.parent.gameObject;
+        //line在兩層後 設定line相關資訊
+        lineName = father_gameObject.name;
+        if (string.Compare(lineName, "Line3") == 0)
+        {
+            line = 3;
+        } else if (string.Compare(lineName, "Line6") == 0)
+        {
+            line = 6;
+        }
+        Debug.Log("line: " + line);
+        father_gameObject = father_gameObject.transform.parent.gameObject;
+        //song在三層後 設定音樂名稱
+        songName = father_gameObject.name;
+        Debug.Log("songName" + songName);
+
+        level = transform.name;
+        Debug.Log("level!" + level);
+        
         // Debug.Log(Path.GetExtension("E:\\user\\Desktop\\musicgame\\musicgame\\musicgame\\Assets\\Audios\\cAudio\\Don't say lazy-cut.mp3"));      
         // Game.SceneController.songDataAsset = Resources.Load<TextAsset>(noteTxt);
 
@@ -33,25 +59,50 @@ public class ListButton : MonoBehaviour
         //while (!www.isDone) { }
         //Game.SceneController.songDataAsset= Resources.Load<TextAsset>(noteTxt);
         //songData.audio = www.GetAudioClip();
-        songData.audio = Resources.Load<AudioClip>("Audios/cAudio/"+ transform.name);
-        if(line == 3)
+        songData.audio = Resources.Load<AudioClip>("Audios/cAudio/" + songName);
+        if (line == 3)
         {
-            songData.Line3SongDataAsset = Resources.Load<TextAsset>("3linetxt/" + transform.name+"/"+ transform.name);
-            Debug.Log("Line3SongDataAsset:" + songData.Line3SongDataAsset.name);
-        }else if (line == 6)
+            songTxt = Resources.Load<TextAsset>("3linetxt/" + songName + "/" + songName + "-" + level);
+            if (songTxt == null)
+            {
+                Debug.Log("not found this level!");
+                //this.gameObject.SetActive(false);
+                //按化處理
+            }
+            else {
+                songData.Line3SongDataAsset = Resources.Load<TextAsset>("3linetxt/" + songName + "/" + songName + "-" + level);
+                Debug.Log("Line3SongDataAsset:" + songData.Line3SongDataAsset.name);
+                // Debug.Log("notetxt:" + songData.);
+            }
+
+        } else if (line == 6)
         {
-            songData.Line6SongDataAsset = Resources.Load<TextAsset>("6linetxt/" + transform.name + "/" + transform.name);
-            Debug.Log("Line6SongDataAsset:" + songData.Line6SongDataAsset.name);
+            songTxt = Resources.Load<TextAsset>("6linetxt/" + songName + "/" + songName + "-" + level);
+            if (songTxt == null)
+            {
+                Debug.Log("not found this level!");
+                //this.gameObject.SetActive(false);
+                //按化處理
+            }
+            else
+            {
+                songData.Line6SongDataAsset = Resources.Load<TextAsset>("6linetxt/" + songName + "/" + songName + "-" + level);
+                Debug.Log("Line6SongDataAsset:" + songData.Line6SongDataAsset.name);
+            }
         }
         Debug.Log("audio:" + songData.audio.name);
-        
-        
-        if(line == 3)
+
+        //轉跳sence
+        if (songTxt != null)
         {
-            SceneManager.LoadScene("Game 3ver");
-        }else if (line == 6)
-        {
-            SceneManager.LoadScene("Game 6ver");
+            if (line == 3)
+            {
+                SceneManager.LoadScene("Game 3ver");
+            }
+            else if (line == 6)
+            {
+                SceneManager.LoadScene("Game 6ver");
+            }
         }
 
         //}
@@ -59,11 +110,46 @@ public class ListButton : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        this.GetComponent<Button>().onClick.AddListener(OnPathClick);
+        /*
+        //取得這首歌的名稱
+        father_gameObject = gameObject.transform.parent.gameObject;
+        father_gameObject = father_gameObject.transform.parent.gameObject;
+        //line在兩層後 設定line相關資訊
+        lineName = father_gameObject.name;
+        if (string.Compare(lineName, "Line3") == 0)
+        {
+            line = 3;
+        }
+        else if (string.Compare(lineName, "Line6") == 0)
+        {
+            line = 6;
+        }
+        Debug.Log("line: " + line);
+        father_gameObject = father_gameObject.transform.parent.gameObject;
+        //song在三層後 設定音樂名稱
+        songName = father_gameObject.name;
+        Debug.Log("songName" + songName);
+
+        level = transform.name;
+        Debug.Log("level!" + level);
+        if (line == 3)
+        {
+            songTxt = Resources.Load<TextAsset>("3linetxt/" + songName + "/" + songName + "-" + level);
+        }else if (line == 6)
+        {
+            songTxt = Resources.Load<TextAsset>("6linetxt/" + songName + "/" + songName + "-" + level);
+        }
+        if (songTxt == null)
+        {
+            this.gameObject.SetActive(false);
+        }
+        */
+            this.GetComponent<Button>().onClick.AddListener(OnPathClick);
+
     }
 
-    // Update is called once per frame
-    void Update()
+            // Update is called once per frame
+            void Update()
     {
 
     }
---
+++ musicgame/Assets/Scripts/List/Test1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject father_gameObject;   //宣告一個GameObject(用來放取得的父物件)。
+
+    void Start()
+    {    //一開始就執行。
+
+        father_gameObject = gameObject.transform.parent.gameObject;
+        father_gameObject = father_gameObject.transform.parent.gameObject;
+        father_gameObject = father_gameObject.transform.parent.gameObject;
+
+        //宣告的物件 = 取得本身的父物件。
+
+
+
+        Debug.Log(father_gameObject.name);   //Debug父物件的名稱。
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
