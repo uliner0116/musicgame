f06614ad99ab9ad686339d29a66961c0ada4d583
z01329125 <z01329125@gmail.com>
2019-10-04 16:18:09 +0800

    選單ex3 試用圖檔成功 (缺按鈕功能)

---
+++ musicgame/Assets/FancyScrollView/Examples/Sources/03_InfiniteScroll/Example03.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace FancyScrollView.Example03
+{
+    public class Example03 : MonoBehaviour
+    {
+        [SerializeField] ScrollView scrollView = default;
+        string[] name = { "I_m Sorry", "Don_t say lazy","Butter-Fly", "LATATA","LOVE","Mirotic","Oh!","One Night In北京","PON PON PON","Roly Poly", "SORRY, SORRY", "Trouble Maker","Tunak Tunak Tun","YES or YES","三國戀","千年之戀", "恋愛サーキュレーション", "恋は渾沌の隷也", "回レ! 雪月花", "夠愛", "夏祭り"};
+        void Start()
+        {
+            var items = Enumerable.Range(0,20)
+                .Select(i => new ItemData(name[i], name[i]))
+                .ToArray();
+
+            scrollView.UpdateData(items);
+            scrollView.SelectCell(0);
+        }
+    }
+}
