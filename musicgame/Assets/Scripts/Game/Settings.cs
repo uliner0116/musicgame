0e5e6dfe6a6e7bb7550a0d19b357c1a889d73665
uliner <52157707+uliner0116@users.noreply.github.com>
2019-10-17 23:15:10 +0800

    3D

---
+++ musicgame/Assets/Scripts/Game/Settings.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public bool _preview { get { return preview; } protected set { preview = value; } }
+    public bool _randomTiming { get { return randomTiming; } }
+
+    public TextMesh comment;
+
+    [HideInInspector]
+    private int Count = 0;
+    [SerializeField]
+    private bool preview;
+
+    [SerializeField]
+    private bool randomTiming;
+
+    void Awake()
+    {
+        comment.text = "";
+        _preview = preview;
+    }
+
+
+
+    public int toTimingID(string key)
+    {
+        var ret = new System.Collections.Generic.Dictionary<string, int>()
+        {
+            {"perfect", 1 },
+            {"great", 2 },
+            {"nice", 3 },
+            {"bad", 0 }
+        };
+        return ret[key];
+    }
+
+    public void Comment(int com)
+    {
+        switch (com)
+        {
+            case 1:
+                Count++;
+                comment.text = "<color=\"#FEFB58\">Perfect!!</color>";
+                comment.gameObject.SetActive(true);
+                break;
+            case 2:
+                Count++;
+                comment.text = "<color=\"#5BFE6F\">Great!!</color>";
+                comment.gameObject.SetActive(true);
+                break;
+            case 3:
+                Count++;
+                comment.text = "Nice!!" ;
+                comment.gameObject.SetActive(true);
+                break;
+            case 0 :
+                Count++;
+                comment.text = "Bad!!" ;
+                comment.gameObject.SetActive(true);
+                break;
+            default:
+                Count = 0;
+                comment.text = "<color=\"red\">Miss</color>" ;
+                comment.gameObject.SetActive(true);
+                break;
+        }
+    }
+}
